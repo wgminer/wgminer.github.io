83f46695401c867355875727b176402c5ee8de21
Will Miner <wgminer@gmail.com>
2024-07-25 19:23:38 -0400

    V1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+document.addEventListener("DOMContentLoaded", () => {
+  const wavingHand = document.getElementById("waving-hand");
+
+  if (wavingHand) {
+    function removeWave() {
+      wavingHand.classList.remove("wave");
+    }
+
+    // Remove the wave animation after 15 seconds
+    setTimeout(removeWave, 15000);
+
+    // Remove the wave animation when hovered over
+    wavingHand.addEventListener("mouseover", removeWave);
+  } else {
+    console.error('Element with id "waving-hand" not found');
+  }
+});
