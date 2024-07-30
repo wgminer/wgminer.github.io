7923f836452b1a35377d0ceeaf722eaacde2ef4b
Will Miner <wgminer@gmail.com>
2024-07-30 17:54:29 -0400

    Nice!

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   const wavingHand = document.getElementById("waving-hand");
+  const helloElement = document.querySelector("h1");
 
-  if (wavingHand) {
-    function removeWave() {
-      wavingHand.classList.remove("wave");
-    }
+  const hellos = ["Hello", "Welcome", "Good to see you!", "Sup", "Yo", "Hola"];
+  let currentHelloIndex = 0;
 
-    // Remove the wave animation after 15 seconds
-    setTimeout(removeWave, 15000);
-
-    // Remove the wave animation when hovered over
-    wavingHand.addEventListener("mouseover", removeWave);
-  } else {
-    console.error('Element with id "waving-hand" not found');
+  function changeHello() {
+    currentHelloIndex = (currentHelloIndex + 1) % hellos.length;
+    helloElement.innerHTML = `<span class="wave" id="waving-hand">👋🏾</span> ${hellos[currentHelloIndex]}`;
   }
+
+  function removeWave() {
+    wavingHand.classList.remove("wave");
+  }
+
+  // Change the hello word every 1 seconds
+  // const helloInterval = setInterval(changeHello, 1000);
+
+  // Remove the wave animation after 15 seconds
+  const removeWaveTimeout = setTimeout(() => {
+    removeWave();
+    // clearInterval(helloInterval); // Stop changing hello words after 15 seconds
+  }, 15000);
+
+  // Remove the wave animation when hovered over
+  wavingHand.addEventListener("mouseover", () => {
+    removeWave();
+    // clearInterval(helloInterval); // Stop changing hello words when hovered over
+    clearTimeout(removeWaveTimeout); // Clear the timeout to avoid removing the wave again
+  });
 });
